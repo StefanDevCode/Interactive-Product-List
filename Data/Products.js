ad7225ed1e47de7aa14de4e95382eb9d89bc749e
StefannCode <stefan.bznr@gmail.com>
2024-11-20 22:17:55 +0100

    Initial static page layout

---
+++ Data/Products.js
@@ -0,0 +1,56 @@
+const Products = [
+  {
+    name: "Waffle",
+    nameDescription: "Waffle With Berries",
+    image: "Product-images/Waffle with Berries.png",
+    price: 650,
+  },
+  {
+    name: "Crème Brûlée",
+    nameDescription: "Vanilla Bean Crème Brûlée",
+    image: "Product-images/Vanilla Bean Crème Brûlée.png",
+    price: 700,
+  },
+  {
+    name: "Macaron",
+    nameDescription: "Macaron Mix of Five",
+    image: "Product-images/Macaron Mix of Five.png",
+    price: 800,
+  },
+  {
+    name: "Tiramisu",
+    nameDescription: "Classic tiramisu",
+    image: "Product-images/Classic Tiramisu.png",
+    price: 550,
+  },
+  {
+    name: "Baklava",
+    nameDescription: "Pistachio Baklava",
+    image: "Product-images/Pistachio Baklava.png",
+    price: 400,
+  },
+  {
+    name: "Pie",
+    nameDescription: "Lemon Meringue Pie",
+    image: "Product-images/Lemon Meringue Pie.png",
+    price: 500,
+  },
+  {
+    name: "Cake",
+    nameDescription: "Red Velvet Cake",
+    image: "Product-images/Red Velvet Cake.png",
+    price: 450,
+  },
+  {
+    name: "Brownie",
+    nameDescription: "Salted Caramel Brownie",
+    image: "Product-images/Salted Caramel Brownie.png",
+    price: 550,
+  },
+  {
+    name: "Panna Cotta",
+    nameDescription: "Vanilla Panna Cotta",
+    image: "Product-images/Vanilla Panna Cotta.png",
+    price: 650,
+  },
+];
